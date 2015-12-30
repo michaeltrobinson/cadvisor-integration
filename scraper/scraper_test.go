8a4e5d3927126fb297ed4ccda2d650d8ca422ae7
Michael Robinson <mrobinson@zvelo.com>
2015-12-30 14:35:36 -0700

    Stubbed out test

---
+++ scraper/scraper_test.go
@@ -0,0 +1,17 @@
+package scraper
+
+import (
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestScraper(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+
+	Convey("Test Scraper", t, func() {
+		// TODO: actually test
+	})
+}
